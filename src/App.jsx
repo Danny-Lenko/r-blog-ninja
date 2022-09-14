f6feb53d2e2465d67f649d9d852c28dab7964f62
Valerii <devdanny.14@gmail.com>
2022-09-14 15:09:10 +0300

    creating a new git repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1 className="app-h1">Hello React App</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
